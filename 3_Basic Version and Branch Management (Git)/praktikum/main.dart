ccbecac72c093639265877403c9f5889a9c0a77e
masnurrm <nurmuhammad448@gmail.com>
2022-08-25 15:20:34 +0700

    feat: add line in main.dart

--- 3_Basic Version and Branch Management (Git)/praktikum/main.dart
+++ 3_Basic Version and Branch Management (Git)/praktikum/main.dart
@@ -1,5 +1,9 @@
 void main() {
   print('Hello, World!');
   print('Hello, Alterra!');
+<<<<<<< Updated upstream
   print('Saya mengedit featureA dari Remote Repository');
+=======
+  print('Hello, saya di featureA. Apa kabar?');
+>>>>>>> Stashed changes
 }
