61fe4ad300596d519b72ff64972a5f308534fc7a
masnurrm <nurmuhammad448@gmail.com>
2022-08-25 15:28:13 +0700

    fix: line issue in featureA

--- 3_Basic Version and Branch Management (Git)/praktikum/main.dart
+++ 3_Basic Version and Branch Management (Git)/praktikum/main.dart
@@ -1,9 +1,6 @@
 void main() {
   print('Hello, World!');
   print('Hello, Alterra!');
-<<<<<<< Updated upstream
   print('Saya mengedit featureA dari Remote Repository');
-=======
   print('Hello, saya di featureA. Apa kabar?');
->>>>>>> Stashed changes
 }
