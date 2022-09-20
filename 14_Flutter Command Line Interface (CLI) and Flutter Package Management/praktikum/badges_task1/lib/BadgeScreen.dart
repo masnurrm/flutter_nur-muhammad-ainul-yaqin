fe8a2e960bfe4128b76b507d922201058316d401
masnurrm <nurmuhammad448@gmail.com>
2022-09-20 16:07:52 +0700

    feat: task 1 2 praktikum section 14

--- 14_Flutter Command Line Interface (CLI) and Flutter Package Management/praktikum/badges_task1/lib/BadgeScreen.dart
+++ 14_Flutter Command Line Interface (CLI) and Flutter Package Management/praktikum/badges_task1/lib/BadgeScreen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 class BadgeScreen extends StatelessWidget {
   const BadgeScreen({super.key});
+  
+  get padding => null;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +16,41 @@ class BadgeScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          
+          children: <Widget> [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Badge(
+                badgeColor: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(100),
+                padding: const EdgeInsets.only(top: 12, bottom: 12, left: 20, right: 20),
+                shape: BadgeShape.square,
+                badgeContent: const Text(
+                  'BADGE',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            // const SizedBox(height:10),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Badge(
+                badgeColor: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(100),
+                padding: EdgeInsets.all(padding ?? 4),
+                shape: BadgeShape.square,
+                badgeContent: const Text(
+                  'BADGE',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
